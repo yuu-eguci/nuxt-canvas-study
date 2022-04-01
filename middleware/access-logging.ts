b4c5e1c9b01b062cfc5157d3103deff38601f324
yuu-eguci <yuu.eguci@gmail.com>
2022-04-01 20:16:21 +0900

    ✨ middleware/access-logging をちょろっと変更

--- middleware/access-logging.ts
+++ middleware/access-logging.ts
@@ -1,9 +1,10 @@
 import { Middleware } from '@nuxt/types'
 
 const myMiddleware: Middleware = (context) => {
-  // console.info(`You access ${route.fullPath}`)
-  console.info('ここは middleware/access-logging')
-  console.info({ context })
+  console.info(JSON.stringify({
+    hereIs: 'middleware/access-logging',
+    fullPathIs: context.route.fullPath
+  }))
 }
 
 export default myMiddleware
