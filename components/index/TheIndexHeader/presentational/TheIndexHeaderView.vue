0bb5f4c89037a40438df32d762499973e704b25d
yuu-eguci <yuu.eguci@gmail.com>
2022-04-01 20:29:20 +0900

    🎨 TheIndexHeaderView 追加

---
+++ components/index/TheIndexHeader/presentational/TheIndexHeaderView.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-alert
+    dense
+    text
+    type="success"
+  >
+    <div>
+      Nuxt.js + Object Style + Vuetify + Canvas + GitHub Pages
+      <v-btn
+        class="ms-2"
+        elevation="2"
+        fab
+        x-small
+        color="teal"
+        href="https://github.com/yuu-eguci/nuxt-canvas-study"
+        target="_blank"
+      >
+        <v-icon>
+          mdi-github
+        </v-icon>
+      </v-btn>
+    </div>
+    <h1>
+      Nuxt-Canvas-Study
+    </h1>
+  </v-alert>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'TheIndexHeaderView'
+})
+</script>
