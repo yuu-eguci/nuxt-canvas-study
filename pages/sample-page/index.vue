27db07f91c2b3ae5113894756f23c79ab49cbfb1
yuu-eguci <yuu.eguci@gmail.com>
2022-04-01 10:25:42 +0900

    🎨 これまで追加した component と utils を実際に使う sample-page

---
+++ pages/sample-page/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <TheSampleContainer />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { foo, bar } from '@/utils/CommonUtils'
+import { Baz } from '@/utils/Baz'
+import TheSampleContainer from '@/components/TheSample/container/TheSampleContainer.vue'
+
+// NOTE: vue-cli と近い使用感を実現するため Object Style で実装します。
+export default Vue.extend({
+  components: {
+    TheSampleContainer
+  },
+  mounted () {
+    // 動作確認用、意味のないコード
+    // const foo: string = ''
+    // console.log(foo)
+
+    // eslint-disable-next-line
+    console.info(foo())
+    console.info(bar())
+    console.info(new Baz(1, 2))
+  }
+})
+</script>
