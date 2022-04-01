45ff7f9447407b14ebc15fe567fac048294d5e66
yuu-eguci <yuu.eguci@gmail.com>
2022-04-01 10:22:36 +0900

    🎨 Component は component.vue ではなく component/container/ComponentContainer.vue と component/presentational/ComponentView.vue で定義する (これが一番目新しかった)

---
+++ components/TheSample/container/TheSampleContainer.vue
@@ -0,0 +1,22 @@
+<script lang="ts">
+import Vue, { CreateElement, VNode } from 'vue'
+import TheSampleView from '../presentational/TheSampleView.vue'
+
+export default Vue.extend({
+  methods: {
+    // API access
+    getRandomData (): string {
+      return 'foo'
+    }
+  },
+  // render (h) { だと、 Property 'getRandomData' does not exist on type 'CombinedVueInstance が出る
+  render (h: CreateElement): VNode {
+    return h(TheSampleView, {
+      props: {
+        getRandomData: this.getRandomData,
+        alan: 'alan'
+      }
+    })
+  }
+})
+</script>
