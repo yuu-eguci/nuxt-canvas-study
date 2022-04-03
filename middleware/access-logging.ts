5e68952451b782ad9bc22e8fe1641931a3f8892e
yuu-eguci <yuu.eguci@gmail.com>
2022-04-03 20:41:22 +0900

    ✨ 開発環境でのみ console.log する plugin を inject してみる

--- middleware/access-logging.ts
+++ middleware/access-logging.ts
@@ -1,7 +1,7 @@
 import { Middleware } from '@nuxt/types'
 
-const myMiddleware: Middleware = (context) => {
-  console.info(JSON.stringify({
+const myMiddleware: Middleware = (context): void => {
+  context.app.$debug(JSON.stringify({
     hereIs: 'middleware/access-logging',
     fullPathIs: context.route.fullPath
   }))
---
+++ plugins/debugLog.ts
@@ -0,0 +1,39 @@
+// NOTE: Plugin の作り方はまだよくわかっていないが、
+//       typescript.nuxtjs.org のコード↓をそのままコピペし、 $debug 用に改造しています。
+//       https://typescript.nuxtjs.org/cookbook/plugins
+import { Plugin } from '@nuxt/types'
+
+declare module 'vue/types/vue' {
+  // this.$debug inside Vue components
+  interface Vue {
+    $debug(...args: any[]): void
+  }
+}
+
+declare module '@nuxt/types' {
+  // nuxtContext.app.$debug inside asyncData, fetch, plugins, middleware, nuxtServerInit
+  interface NuxtAppOptions {
+    $debug(...args: any[]): void
+  }
+  // nuxtContext.$debug
+  interface Context {
+    $debug(...args: any[]): void
+  }
+}
+
+declare module 'vuex/types/index' {
+  // this.$debug inside Vuex stores
+  // FIXME: どう解決したらよいのかわかんない。
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  interface Store<S> {
+    $debug(...args: any[]): void
+  }
+}
+
+// NOTE: _ をつけると、 "使わない引数なので気にしないでね" という意味になる。らしい。
+const myPlugin: Plugin = (_context, inject) => {
+  // eslint-disable-next-line no-console
+  inject('debug', (...args: any[]) => console.log(...args))
+}
+
+export default myPlugin
