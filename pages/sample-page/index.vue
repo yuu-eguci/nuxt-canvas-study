5ea5bd628f0355ead4b48f3d781d73c9a9a0015c
yuu-eguci <yuu.eguci@gmail.com>
2022-04-03 19:09:57 +0900

    🔥 要らないファイル

--- pages/sample-page/index.vue
+++ pages/sample-page/index.vue
@@ -12,6 +12,7 @@ import TheSampleContainer from '@/components/TheSample/container/TheSampleContai
 
 // NOTE: vue-cli と近い使用感を実現するため Object Style で実装します。
 export default Vue.extend({
+  name: 'SamplePage',
   components: {
     TheSampleContainer
   },
